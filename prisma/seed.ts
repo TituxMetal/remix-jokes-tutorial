6f3a77f2fd8a0e594eaac695d05a4e197583f255
Titux Metal (MBP) <github@lgdweb.fr>
2023-03-23 11:47:11 +0100

    Add a seed file that creates initial data

---
+++ prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from '@prisma/client'
+
+const db = new PrismaClient()
+
+const getJokes = () => [
+  {
+    name: 'Road worker',
+    content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`
+  },
+  {
+    name: 'Frisbee',
+    content: `I was wondering why the frisbee was getting bigger, then it hit me.`
+  },
+  {
+    name: 'Trees',
+    content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`
+  },
+  {
+    name: 'Skeletons',
+    content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`
+  },
+  {
+    name: 'Hippos',
+    content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`
+  },
+  {
+    name: 'Dinner',
+    content: `What did one plate say to the other plate? Dinner is on me!`
+  },
+  {
+    name: 'Elevator',
+    content: `My first time using an elevator was an uplifting experience. The second time let me down.`
+  }
+]
+
+const resetJokesDb = async () =>
+  db.joke
+    .deleteMany()
+    .then(data => console.log('succefully deleted data', data))
+
+const createJokes = async () =>
+  getJokes().map(joke =>
+    db.joke
+      .create({ data: joke })
+      .then(data => console.log('joke created', data))
+  )
+
+const seed = async () => {
+  Promise.all([resetJokesDb(), createJokes()])
+}
+
+seed()
+  .then(async () => {
+    await db.$disconnect()
+  })
+  .catch(async e => {
+    console.error(e)
+    await db.$disconnect()
+    process.exit(1)
+  })
