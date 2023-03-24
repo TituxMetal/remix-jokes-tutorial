514e9291017b2fc54b16e243e85a734044ef00f4
Titux Metal (MBP) <github@lgdweb.fr>
2023-03-24 11:11:32 +0100

    Update seed file; add a user and link it to jokes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,59 +2,70 @@ import { PrismaClient } from '@prisma/client'
 
 const db = new PrismaClient()
 
-const getJokes = () => [
-  {
-    name: 'Road worker',
-    content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`
-  },
-  {
-    name: 'Frisbee',
-    content: `I was wondering why the frisbee was getting bigger, then it hit me.`
-  },
-  {
-    name: 'Trees',
-    content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`
-  },
-  {
-    name: 'Skeletons',
-    content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`
-  },
-  {
-    name: 'Hippos',
-    content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`
-  },
-  {
-    name: 'Dinner',
-    content: `What did one plate say to the other plate? Dinner is on me!`
-  },
-  {
-    name: 'Elevator',
-    content: `My first time using an elevator was an uplifting experience. The second time let me down.`
-  }
-]
+const getJokes = () => {
+  // shout-out to https://icanhazdadjoke.com/
+
+  return [
+    {
+      name: 'Road worker',
+      content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`
+    },
+    {
+      name: 'Frisbee',
+      content: `I was wondering why the frisbee was getting bigger, then it hit me.`
+    },
+    {
+      name: 'Trees',
+      content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`
+    },
+    {
+      name: 'Skeletons',
+      content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`
+    },
+    {
+      name: 'Hippos',
+      content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`
+    },
+    {
+      name: 'Dinner',
+      content: `What did one plate say to the other plate? Dinner is on me!`
+    },
+    {
+      name: 'Elevator',
+      content: `My first time using an elevator was an uplifting experience. The second time let me down.`
+    }
+  ]
+}
 
 const resetJokesDb = async () =>
   db.joke
     .deleteMany()
-    .then(data => console.log('succefully deleted data', data))
+    .then(({ count }) => console.log(`succefully deleted jokes: ${count}`))
 
-const createJokes = async () =>
-  getJokes().map(joke =>
-    db.joke
-      .create({ data: joke })
-      .then(data => console.log('joke created', data))
-  )
+const resetUserDb = async () =>
+  db.user
+    .deleteMany()
+    .then(({ count }) => console.log(`succefully deleted users: ${count}`))
 
 const seed = async () => {
-  Promise.all([resetJokesDb(), createJokes()])
+  await Promise.all([resetJokesDb(), resetUserDb()])
+  const kody = await db.user.create({
+    data: {
+      username: 'kody',
+      // this is a hashed version of "twixrox"
+      hash: '$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u'
+    }
+  })
+
+  console.log(`User created ${kody.username}`)
+
+  await Promise.all(
+    getJokes().map(jokeData =>
+      db.joke
+        .create({ data: { jokesterId: kody.id, ...jokeData } })
+        .then(data => console.log(data.name))
+    )
+  )
 }
 
 seed()
-  .then(async () => {
-    await db.$disconnect()
-  })
-  .catch(async e => {
-    console.error(e)
-    await db.$disconnect()
-    process.exit(1)
-  })
