9cd1d922adbab9106f05d28f1fa387d327df1635
Titux Metal (MBP) <github@lgdweb.fr>
2023-03-24 08:07:50 +0100

    Implement the random joke route

--- src/routes/jokes/index.tsx
+++ src/routes/jokes/index.tsx
@@ -1,8 +1,27 @@
+import { json } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+
+import { prisma } from '~/lib'
+
+export const loader = async () => {
+  const count = await prisma.joke.count()
+  const randomRowNumber = Math.floor(Math.random() * count)
+  const [randomJoke] = await prisma.joke.findMany({
+    take: 1,
+    skip: randomRowNumber
+  })
+
+  return json({ randomJoke })
+}
+
 const JokesIndexRoute = () => {
+  const { randomJoke } = useLoaderData<typeof loader>()
+
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{randomJoke.content}</p>
+      <Link to={randomJoke.id}>"{randomJoke.name}" Permalink</Link>
     </div>
   )
 }
