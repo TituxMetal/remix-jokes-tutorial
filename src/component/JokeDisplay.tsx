82f033a1de08585f692ba85749c1a9415e3f0537
Titux Metal (MBP) <github@lgdweb.fr>
2023-03-26 16:18:43 +0200

    Add a joke display reusable component

---
+++ src/component/JokeDisplay.tsx
@@ -0,0 +1,40 @@
+import type { Joke } from '@prisma/client'
+import { Form, Link } from '@remix-run/react'
+import type { FC } from 'react'
+
+interface JokeDisplayProps {
+  isOwner: boolean
+  joke: Pick<Joke, 'content' | 'name'>
+  canDelete?: boolean
+}
+
+const JokeDisplay: FC<JokeDisplayProps> = ({
+  isOwner,
+  joke,
+  canDelete = true
+}) => {
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{joke.content}</p>
+      <Link prefetch='intent' to='.'>
+        {joke.name} Permalink
+      </Link>
+      {isOwner && (
+        <Form method='post'>
+          <button
+            type='submit'
+            disabled={!canDelete}
+            className='button'
+            name='intent'
+            value='delete'
+          >
+            Delete
+          </button>
+        </Form>
+      )}
+    </div>
+  )
+}
+
+export default JokeDisplay
